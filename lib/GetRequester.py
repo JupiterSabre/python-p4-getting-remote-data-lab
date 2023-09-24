ef5106dd6b5dca7a039e47abcef8767554cbe832
Zulai Romero <zulairomero@gmail.com>
2023-09-24 01:46:40 -0400

    completed response

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,4 +1,4 @@
-import requests
+import requests as req
 import json
 
 class GetRequester:
@@ -7,7 +7,29 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        res =  req.get(self.url)
+        return res.content
 
     def load_json(self):
-        pass
+
+        return json.loads(self.get_response_body())
+
+
+# import requests
+# import json
+
+# class GetRequester:
+
+#     def __init__(self, url):
+#         self.url = url
+
+#     def get_response_body(self):
+
+#         response = requests.get(self.url)
+#         return response.content
+
+#     def load_json(self):
+#         return json.loads(self.get_response_body())
+    
+# requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+# print(requester.load_json())
